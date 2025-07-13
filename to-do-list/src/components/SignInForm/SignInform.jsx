aef087a6aee18496d6e816fd9a06dfb83391c640
Mustafa Anis Kanorwala <mustafa.kanorwala@newgensoft.com>
2025-07-13 21:34:25 +0530

    Login/Register integration with Backend done

--- to-do-list/src/components/SignInForm/SignInform.jsx
+++ to-do-list/src/components/SignInForm/SignInform.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './SignInForm.scss';
 import Button from '../Button/Button';
 import FormInput from '../form-input/form-input';
@@ -22,9 +23,39 @@ const SignInForm = () => {
         
     }
 
+    const navigate = useNavigate();
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+
+        try{
+            const response = await fetch('http://localhost:8080/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formFields)
+            });
+
+            if(!response){
+                throw new Error('Invalid Username/Password');
+            }
+
+            const data = await response.json();
+            const token = data.token;            
+
+            localStorage.setItem('jwtToken',token);
+
+            resetFormFields();
+
+        }catch(error){
+            console.error(error);
+        }
+    };
+
     return(
         <div className='sign-in-container'>
-            <form>
+            <form onSubmit={handleSubmit} autoComplete='off'>
                 <FormInput
                     type="text"
                     name="username"
--- to-do-list/src/components/SignUpForm/SignUpForm.jsx
+++ to-do-list/src/components/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import FormInput from '../form-input/form-input';
 import Button from '../Button/Button';
 import './SignUpForm.scss';
@@ -22,10 +23,40 @@ const SignUpForm = () => {
         setFormFields({...formFields, [name]:value});
         
     }
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+
+        try{
+            const response = await fetch('http://localhost:8080/api/auth/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formFields)
+            });
+
+            if(!response){
+                throw new Error('Failed to Register the User');
+            }
+
+            const data = await response.json();
+            const token = data.token;            
+
+            localStorage.setItem('jwtToken',token);
+
+            resetFormFields();
+
+        }catch(error){
+            console.error(error);
+        }
+    };
     
     return(
         <div className='sign-up-container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <FormInput
                     type="text"
                     name="username"
