f7b31b1b121844d7f2ae3c476a1b5905e258229d
Mustafa Anis Kanorwala <mustafa.kanorwala@newgensoft.com>
2025-07-14 23:37:33 +0530

    Completed todo card component and its fetching, deleting and status toggling

--- to-do-list/src/components/TO-DO-Item/To-Do-Item.jsx
+++ to-do-list/src/components/TO-DO-Item/To-Do-Item.jsx
@@ -1,8 +1,87 @@
 import './To-Do-Item.scss';
 
-const TodoItem = () => {
+const TodoItem = ({todo, refreshTodos, onToggle}) => {    
+
+    const {title,description,createdAt,dueDate,status} = todo;
+
+    const handleToggle = async () => {
+        try{            
+            const url = `http://localhost:8080/todo/${todo.id}/toggle`;
+
+            const response = await fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
+                }
+            });
+
+            if(!response.ok){
+                throw new Error('Failed to change the status.')
+            }
+            
+            onToggle(todo.id);
+            
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    const handleEdit = () => {
+        
+    }
+
+    const handleDelete = async () => {
+        try{            
+            const url = `http://localhost:8080/todo/${todo.id}`;
+
+            const response = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
+                }
+            });
+
+            if(response.ok){
+                alert('ToDo deleted successfully.');
+                refreshTodos();
+            }else{
+                alert('Failed to delete Todo.')
+            }       
+            
+        }catch(error){
+            console.error(error);
+        }
+    }
+
     return(
-        <h4>Todo Item</h4>
+        <div id='todo-card'>
+            <h2 id='todo-title'>{title}</h2>
+            <p id='todo-desc'>{description}</p>
+
+            <div id='todo-dates'>
+                <span id='created-date'>Created: {createdAt}</span>
+                <span id='due-date'>Due: {dueDate}</span>
+            </div>
+
+            <div id='todo-footer'>
+                <div className="status-toggle">
+                    <label className="toggle-switch">
+                        <input
+                            type="checkbox"
+                            checked={status === 'Completed'}
+                            onChange={handleToggle}
+                        />
+                        <span className="slider"></span>
+                    </label>
+                    <span className="status-text">{status}</span>
+                </div>
+
+                <div className="actions">
+                    <button onClick={handleEdit}>✏️</button>
+                    <button onClick={handleDelete}>🗑️</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- to-do-list/src/routes/Dashboard/Dashboard.jsx
+++ to-do-list/src/routes/Dashboard/Dashboard.jsx
@@ -1,16 +1,65 @@
+import { useState, useEffect } from 'react';
 import Title from '../../components/title/title.component';
 import Footer from '../../components/Footer/Footer';
+import TodoItem from '../../components/TO-DO-Item/To-Do-Item';
 import { getUsernameFromToken } from '../../utils/jwtUtils';
 import './Dashboard.scss';
 
 const Dashboard = () => {
 
+    const [todos, setTodos] = useState([]);
+
+    const fetchTodos = async () => {
+        
+        try{
+            const response = await fetch('http://localhost:8080/todo', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`,
+                    'Content-Type': 'application/Json'
+                }
+            });
+
+            if(!response.ok)
+                throw new Error('Failed to fetch Todos');
+
+            const data = await response.json();
+            setTodos(data);          
+            
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    const handleToggleLocal = (id) => {
+        setTodos(prev =>
+            prev.map(todo =>
+                todo.id === id 
+                    ? {...todo, status: todo.status === 'Completed' ? 'Pending' : 'Completed'}
+                    : todo
+            )
+        );
+    };
+
     const username = getUsernameFromToken();
-    
+
+    useEffect(() => {
+        fetchTodos();
+    },[]);
+
     return(
         <div id='dashboard-container'>
             <Title user={username} shrink={'login'}/>
-            
+            <div id='todo-container'>
+                {todos.map((todo, index) => (
+                    <TodoItem 
+                        key={index} 
+                        todo={todo} 
+                        refreshTodos={fetchTodos}
+                        onToggle={handleToggleLocal}
+                    />
+                ))}
+            </div>
             <Footer />
         </div>
     )
